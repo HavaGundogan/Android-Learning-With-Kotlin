b0935f8942d0d1efbb675f377621189e16521850
Hava GÜNDOĞAN <76647908+HavaGundogan@users.noreply.github.com>
2023-08-13 01:29:29 +0300

    create viewbinding

--- AndroidViewBindingExample/app/src/main/java/com/gundogan/androidviewbindingexample/MainActivity.kt
+++ AndroidViewBindingExample/app/src/main/java/com/gundogan/androidviewbindingexample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.gundogan.androidviewbindingexample
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,13 +11,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.gundogan.androidviewbindingexample.databinding.MainActivityBinding
 import com.gundogan.androidviewbindingexample.ui.theme.AndroidViewBindingExampleTheme
 
+
+//findviewvbyid kötü bir şey çünkü R.id her çalıştığında arka planda programı yoruyor.Gezerek buluyor.
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: MainActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)//bunu nasıl unuttum?
-        }
+        binding = MainActivityBinding.inflate(layoutInflater)
+        val view = binding.root //görünümü aldı ve birbirine eşitledi
+        setContentView(view)
+        
+
+
+
+
+    }
 
 }
 
