cffdd745ae36c7676b8581deaa33c026ea1a5d36
Hava GÜNDOĞAN <76647908+HavaGundogan@users.noreply.github.com>
2023-08-15 23:45:40 +0300

    Create SharedPreference

--- StoringData/app/src/main/java/com/gundogan/storingdata/MainActivity.kt
+++ StoringData/app/src/main/java/com/gundogan/storingdata/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.gundogan.storingdata
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,37 +12,62 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.gundogan.storingdata.databinding.ActivityMainBinding
 import com.gundogan.storingdata.ui.theme.StoringDataTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPref:SharedPreferences
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            StoringDataTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        val view =binding.root
+        setContentView(view)
+        //shared preferences --- bir xml dosyası-- key/value
+      sharedPref= getSharedPreferences("com.gundogan.storingdata", MODE_PRIVATE)//oluşturacağım xml e sadece benim uygulamam oluşacak.
+
+        val userAgePref = sharedPref.getInt("age",-1)
+
+        if (userAgePref == -1){
+            binding.textView.text= "yourage: "
+
+        }else{
+            binding.textView.text="your age: ${userAgePref}"
         }
+
     }
+
+    fun save(view: View){
+     val myAge=   binding.editText.text.toString().toIntOrNull()
+
+     if(myAge!= null){
+         binding.textView.text="your age: "+myAge
+         sharedPref.edit().putInt("age",myAge).apply()
+
+     }
+
+
+
+
+
+    }
+    fun delete(view: View){
+
+        val userAgePref =sharedPref.getInt("age",-1)
+
+        if (userAgePref!=-1){
+            sharedPref.edit().remove("age").apply()
+            binding.textView.text="your age "
+
+        }else{
+
+        }
+
+    }
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StoringDataTheme {
-        Greeting("Android")
-    }
-}
